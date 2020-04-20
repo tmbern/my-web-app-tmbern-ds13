fb8d74fc9cb2cfc07e8e6958e24447f9641d1886
tmbern <tmbern42@gmail.com>
2020-04-20 12:27:39 -0600

    migrate database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -16,12 +16,19 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
 # application factory pattern
 def create_app():
     app = Flask(__name__)
+
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_13.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://///home/tmbern/workspace/repos/lambda/my-web-app-tmbern-ds13/web_app_13.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
